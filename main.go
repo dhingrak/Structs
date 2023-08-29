44879c297f3fb085e9d28d8f39c3daf8da49bbae
Kunal Dhingra <kdhingra55@gmail.com>
2023-08-29 18:32:44 -0400

    first commit

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contactInfo
+}
+
+func main() {
+
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contactInfo: contactInfo{
+			email:   "jimparty@gmail.com",
+			zipCode: 94000,
+		},
+	}
+
+	jim.print()
+	jim.updateName("Alex")
+	jim.print()
+
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
+
+func (p *person) updateName(newFirstName string) {
+	p.firstName = newFirstName
+}
---
+++ main_test.go
@@ -0,0 +1,13 @@
+package main
+
+import "testing"
+
+func TestIsWorking(t *testing.T) {
+
+	result := true
+
+	if result != true {
+		t.Errorf("Assert failed, Expected true, got False")
+	}
+
+}
