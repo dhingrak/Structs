bb8acaada6b4a81514cf8f98d57bbe7e29ac14e4
Kunal Dhingra <kdhingra55@gmail.com>
2023-09-11 18:47:39 -0400

    testing pulumi script

--- main_test.go
+++ main_test.go
@@ -7,7 +7,7 @@ func TestIsWorking(t *testing.T) {
 	result := true
 
 	if result != true {
-		t.Errorf("Assert failed, Expected true, got False, hey pulumi script")
+		t.Errorf("Assert failed, Expected true, got False, hey pulumi script testing")
 	}
 
 }
