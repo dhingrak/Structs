98874efeb6bdfb8c43c0e396cdf4546fd30086ef
Kunal Dhingra <kdhingra55@gmail.com>
2023-09-25 18:29:59 -0400

    Testing new ubunut runner

--- main_test.go
+++ main_test.go
@@ -7,7 +7,7 @@ func TestIsWorking(t *testing.T) {
 	result := true
 
 	if result != true {
-		t.Errorf("Assert failed, Expected true, got Falseeeeeee")
+		t.Errorf("Assert failed, Expected true, got False")
 	}
 
 }
